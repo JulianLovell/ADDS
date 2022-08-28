3669f48c9d07476a60ab489adbcb16f8b653ff0f
Julian <jlovell108@gmail.com>
2022-08-28 17:55:37 +0930

    prac4

--- main.cpp
+++ main.cpp
@@ -1,307 +1,101 @@
-#include <iostream>
-
-#include <cmath>
-
-#include <vector>
-
-#include <string>
-
-#include <bits/stdc++.h>
-
+#include<iostream>
+#include<sstream>
 using namespace std;
 
 class Reverse
-
 {
-
 public:
 
-int reverseUtilDigit(int n)
-
+int reverseDigit(int v)
 {
-
-int digit = (int) log10(n);
-
-if(n < 0)
-
+if(v < 0)
+{
 return -1;
-
-else if(n == 0)
-
-return 0;
-
-return ((n % 10 * pow(10, digit)) + reverseUtilDigit(n / 10));
-
+}
+static int newNumber = 0;
+static int multiplier = 1;
+if(v > 0)
+{
+reverseDigit(v/10);
+newNumber += (v%10) * multiplier;
+multiplier *= 10;
+}
+return newNumber;
 }
 
-int reverseDigit(int n)
-
+string reverseString(string s)
 {
+static char *newString = new char[s.length()+1];
 
-int rev = reverseUtilDigit(n);
+reverseStringUtil(s,0,newString);
+newString[s.length()] = '\0';
 
-string reversed = to_string(rev);
-
-for(int i = 0; i < reversed.length() - 1; i++)
-
-{
-
-if(reversed[i] == '0' && reversed[i + 1] != '0')
-
-reversed[i] = '\0';
-
+return newString;
 }
 
-return stod(reversed);
-
-}
-
-string reverseStringUtil(string str ,int size)
-
+void reverseStringUtil(string s, int pos, char ps[])
 {
-
-if (size == -1)
-
-return "";
-
-else
-
-{
-
-char a;
-
-a=str[size];
-
-if (' ' == a )
-
-return reverseStringUtil(str,size-1);
-
-else
-
-return a + reverseStringUtil(str,size-1);
-
-}
-
-}
-
-string reverseString(string letters)
-
-{
-
-string rev = reverseStringUtil(letters, letters.length());
-
-return (rev);
-
+if(s[pos] == '\0')
+return;
+reverseStringUtil(s,pos+1,ps);
+int index = s.length()-pos-1;
+ps[index] = s[pos];
 }
 
 };
 
-class Fibonacci
-
+class TruckLoads
 {
-
 public:
-
-int getNthFibonacci(int n)
-
+int CalculateTruckLoads(int n)
 {
+if(n == 0) return 0;
+if(n == 1 || n == 2) return 1;
 
-if(n < 0)
-
-return -1;
-
-else if(n ==0 || n == 1)
-
-return n;
-
-else
-
-return(getNthFibonacci(n - 1) + getNthFibonacci(n - 2));
-
+return CalculateTruckLoads(n-1) + CalculateTruckLoads(n-2);
 }
-
 };
 
-class EfficientFibonacci
-
+class EfficientTruckLoads
 {
-
-private:
-
-vector<int> calculatedNthNumbers;
-
-vector<int> calculatedNumbers;
+static int *tl;
 
 public:
-
-int getNthFibonacciUtil(int n)
-
+EfficientTruckLoads(int n)
 {
-
-if(n < 0)
-
-return -1;
-
-else if(n ==0 || n == 1)
-
-return n;
-
-else
-
-return(getNthFibonacciUtil(n - 1) + getNthFibonacciUtil(n - 2));
-
+tl = new int[n];
 }
 
-int getNthFibonacci(int n)
-
+int CalculateEfficientTruckLoads(int n)
 {
 
-bool found = false;
+if(n == 1 || n == 2) return 1;
 
-int nthIndex = -1;
+if(tl[n] > 0)
+return tl[n];
 
-for(int i = 0; i < calculatedNthNumbers.size(); i++)
-
-{
-
-if(calculatedNthNumbers[i] == n)
-
-{
-
-found = true;
-
-nthIndex = i;
-
-break;
-
+return tl[n] = CalculateEfficientTruckLoads(n-1) + CalculateEfficientTruckLoads(n-2);
 }
-
-}
-
-if(found)
-
-{
-
-return(calculatedNumbers[nthIndex]);
-
-}
-
-else
-
-{
-
-calculatedNthNumbers.push_back(n);
-
-int fibo = getNthFibonacciUtil(n);
-
-calculatedNumbers.push_back(fibo);
-
-return fibo;
-
-}
-
-}
-
 };
+static int i [] = {0};
+int* EfficientTruckLoads :: tl = i;
 
-bool isNumber(string s);
 
 int main()
-
 {
-
-string reverseDigit = "", reverseString = "", fibo = "", efficientFibo = "", out = "";
-
-int digitToReverse;
-
-string stringToReverse;
-
-int nthFibonacci, nthEfficientFibonacci;
-
-string input1, input2, input3, input4;
-
-cout << endl << "Input: ";
-
-cin >> input1 >> stringToReverse >> input3 >> input4;
-
-if(isNumber(input1))
-
-{
-
-digitToReverse = stod(input1);
-
-if(Reverse().reverseDigit(digitToReverse) == -1)
-
-reverseDigit = "ERROR";
-
+int i,n1,n2;
+string s;
+cin >> i >> s >> n1 >> n2;
+Reverse *r = new Reverse();
+TruckLoads* f = new TruckLoads();
+EfficientTruckLoads *e = new EfficientTruckLoads(n2);
+int p =r->reverseDigit(i);
+if(p == -1)
+cout << "ERROR";
 else
+cout << p;
+cout <<" "<< r->reverseString(s)<<" " << f->CalculateTruckLoads(n1) << " "<< e->CalculateEfficientTruckLoads(n2);
 
-reverseDigit = to_string(Reverse().reverseDigit(digitToReverse));
-
-}
-
-else
-
-reverseDigit = "ERROR";
-
-reverseString = Reverse().reverseString(stringToReverse);
-
-if(isNumber(input3))
-
-{
-
-nthFibonacci = stod(input3);
-
-if(Fibonacci().getNthFibonacci(nthFibonacci) == -1)
-
-fibo = "ERROR";
-
-else
-
-fibo = to_string(Fibonacci().getNthFibonacci(nthFibonacci));
-
-}
-
-else
-
-fibo = "ERROR";
-
-if(isNumber(input4))
-
-{
-
-nthEfficientFibonacci = stod(input4);
-
-if(EfficientFibonacci().getNthFibonacci(nthEfficientFibonacci) == -1)
-
-efficientFibo = "ERROR";
-
-else
-
-efficientFibo = to_string(EfficientFibonacci().getNthFibonacci(nthEfficientFibonacci));
-
-}
-
-else
-
-efficientFibo = "ERROR";
-
-cout << "Output: " << reverseDigit << " " << reverseString << " " << fibo << " " << efficientFibo << endl;
 
 return 0;
-
-}
-
-bool isNumber(string s)
-
-{
-
-for (int i = 0; i < s.length(); i++)
-
-if (isdigit(s[i]) == false)
-
-return false;
-
-return true;
-
 }
